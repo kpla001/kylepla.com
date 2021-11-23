80b5ff2beeb624210eb134cac726590b80e5c6f6
kpla001 <kpla001@fiu.edu>
2021-11-23 15:41:34 -0500

    (prod): add project 2 song bird to portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -25,12 +25,12 @@ export default function Portfolio() {
                 <div className="portfolioMain">
 
                     <section className="portfolioHeader">
-                        <h1 className="portfolioTitle">Projects</h1>
+                        <h1 className="portfolioTitle">Portfolio</h1>
                     </section>
                     <main>
 
                     <div className="portfolioWrap">
-                        <h4 className="projectTitle">Project 1: Free Lunch</h4>
+                        <h4 className="projectTitle"> Project 1: Free Lunch</h4>
                         <Image 
                         className="screenShot"
                         src='/FreeLunchScreenShot.png'
@@ -45,13 +45,30 @@ export default function Portfolio() {
                             </a>
                             <a href="https://kpla001.github.io/Free-Lunch-Ironhack-Game/" 
                             className="demoLink">
-                            <i className="fas fa-play"></i> Live Demo
+                            <i className="fas fa-play"></i> Live Demo*
                             </a>
                         </div>
-
-                        <div className="links">
-
+                        <hr/>
+                        <br/>
+                        <h4 className="projectTitle"> Project 2: Song Bird</h4>
+                        <Image 
+                        className="screenShot"
+                        src='/SongBirdScreenShot.png'
+                        alt='icon' 
+                        width={3524 /4}
+                        height={1884 /4}
+                        />
+                        <div className="projectLinks">
+                            <a href="https://github.com/rgonzalez09/song-bird" 
+                            className="githubLink">
+                            <i className="fas fa-code-branch"></i> Github
+                            </a>
+                            <a href="https://song-bird-discover-music.herokuapp.com" 
+                            className="demoLink">
+                            <i className="fas fa-play"></i> Live Demo*
+                            </a>
                         </div>
+                        
 
                     </div>
                     </main>
