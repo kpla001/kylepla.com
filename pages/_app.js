356fea810aef8adcec51e0e10e4a49f789dc1f71
kpla001 <kpla001@fiu.edu>
2021-11-22 19:48:45 -0500

    (create): create portfolio page and insert title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.scss'
 import '../styles/resume.scss'
+import '../styles/portfolio.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,8 +5,31 @@ import Link from 'next/link'
 
 export default function Portfolio() {
     return (
-        <div id="portfolioPage">
-            
-        </div>
+        <div id="portfolio-page">
+            <Head>
+                <title>Kyle Pla - Portfolio</title>
+
+                <meta name="viewport" content="width=device-width"/>
+                <meta name="description" content="Kyle Robert Isiah Pla - Development Portfolio"/>
+                <meta charset="UTF-8"/> 
+            </Head>
+
+    <body className="portfolioWrap">
+        <main className="portfolioMain">
+                <section className="profile">
+                <h1 className="portfolioTitle">Projects</h1>
+                <h4 className="projectTitle">Software Developer | Electronic Music Producer</h4>
+                </section>
+
+                
+
+                <div className="links">
+                    <ul>
+                    
+                    </ul>
+                </div>
+        </main>
+    </body>
+</div>
     )
 }
